6f985da97cec0415b81c094a166858d93efb270b
hongducdev <contact.hongduc@gmail.com>
2024-05-22 09:33:23 +0700

    chore: Update layout component and add container div

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <div className="p-10">{children}</div>
         </ThemeProvider>
       </body>
     </html>

cfb2d9def60acf6636beb00324f9661a1bec0e0a
hongducdev <contact.hongduc@gmail.com>
2024-05-24 14:45:39 +0700

    Refactor BentoGridItem component to add line-clamp-1 class
    This commit modifies the BentoGridItem component in the bento-grid.tsx file. It adds the line-clamp-1 class to the div element, which limits the number of lines displayed to 1. This change improves the visual presentation of the title in the grid item.

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -59,7 +59,7 @@ export const BentoGridItem = ({
               </div>
             </Link>
           ) : (
-            <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+            <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2 line-clamp-1">
               {title}
             </div>
           )}

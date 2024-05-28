e8148d2a63c2bf3ba4659213f4364e1bc39308ed
hongducdev <contact.hongduc@gmail.com>
2024-05-28 21:20:30 +0700

    refactor: Remove unused code and update component names in page.tsx and [tagName]/page.tsx
    This commit refactors the code in page.tsx and [tagName]/page.tsx files. It removes unused imports, functions, and variables. It also updates component names to improve clarity and consistency. These changes enhance the codebase by reducing complexity and improving maintainability.

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,57 @@
-import { Tag } from "@/@types/schema";
-import { getPublishedPosts, getTags } from "@/apis";
-import { Badge } from "@/components/ui/badge";
-import ListPostCard from "./_components/list-post-card";
+// import { Tag } from "@/@types/schema";
+// import { getPublishedPosts, getTags } from "@/apis";
+// import { Badge } from "@/components/ui/badge";
+// import ListPostCard from "./_components/list-post-card";
 
-export const revalidate = 60;
+// export const revalidate = 60;
 
-const fetchPosts = async () => {
-  try {
-    const response = await getPublishedPosts();
-    return response;
-  } catch (error) {
-    console.error(error);
-  }
-};
+// const fetchPosts = async () => {
+//   try {
+//     const response = await getPublishedPosts();
+//     return response;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
 
-const fetchTags = async () => {
-  try {
-    const response = await getTags();
-    return response;
-  } catch (error) {
-    console.error(error);
-  }
-};
+// const fetchTags = async () => {
+//   try {
+//     const response = await getTags();
+//     return response;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
 
-const MainPage = async () => {
-  const posts = await fetchPosts();
-  const tags = await fetchTags();
+// const MainPage = async () => {
+//   const posts = await fetchPosts();
+//   const tags = await fetchTags();
 
+//   return (
+//     <div className="max-w-7xl mx-auto px-2 lg:px-0">
+//       <div className="my-4 flex flex-row space-x-3">
+//         {tags.map((tag: Tag) => (
+//           <Badge
+//             key={tag.id}
+//             className="text-base"
+//             href={`${process.env.BASE_URL}/tag/${tag.name}`}
+//           >
+//             #{tag.name}
+//           </Badge>
+//         ))}
+//       </div>
+//       <ListPostCard posts={posts} />
+//     </div>
+//   );
+// };
+
+// export default MainPage;
+import React from 'react'
+
+const Page = () => {
   return (
-    <div className="max-w-7xl mx-auto px-2 lg:px-0">
-      <div className="my-4 flex flex-row space-x-3">
-        {tags.map((tag: Tag) => (
-          <Badge
-            key={tag.id}
-            className="text-base"
-            href={`${process.env.BASE_URL}/tag/${tag.name}`}
-          >
-            #{tag.name}
-          </Badge>
-        ))}
-      </div>
-      <ListPostCard posts={posts} />
-    </div>
-  );
-};
+    <div>Page</div>
+  )
+}
 
-export default MainPage;
+export default Page

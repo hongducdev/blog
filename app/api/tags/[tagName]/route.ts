52743f92e7b90fa903d3a26132ab05e9e8d488c0
hongducdev <contact.hongduc@gmail.com>
2024-06-09 14:41:39 +0700

    refactor: Uncomment getPostsByTag function in APIs module
    The getPostsByTag function in the APIs module was previously commented out. This commit removes the commented code and uncomment the function, allowing retrieval of posts based on a specific tag name. This improves the functionality and maintainability of the codebase.

---
+++ app/api/tags/[tagName]/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prismadb";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { tagName: string } }
+) {
+  const tagName = params.tagName;
+
+  if (!tagName) {
+    return NextResponse.json({ error: "Tag is required" }, { status: 400 });
+  }
+
+  try {
+    const tag = await prisma.tag.findUnique({
+      where: {
+        tagName: tagName,
+      },
+    });
+
+    if (!tag) {
+      return NextResponse.json({ error: "Tag not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(tag);
+  } catch (error: unknown) {
+    const errorMessage =
+      error instanceof Error ? error.message : "An unknown error occurred";
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
+  }
+}

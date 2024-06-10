82b1c0e6b9425c72db65f872a136bd5c6e28f27d
hongducdev <contact.hongduc@gmail.com>
2024-06-10 20:11:23 +0700

    refactor: Update login page to use LoginForm component
    The login page in the app/(auth)/login/page.tsx file has been refactored to use the LoginForm component instead of manually rendering the login form. This change improves code organization and maintainability by separating the login form logic into its own component.

---
+++ app/(auth)/login/_component/login-form.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Github from "@/components/icons/Github";
+import { signIn } from "next-auth/react";
+
+const LoginForm = () => {
+  const [loading, setLoading] = useState(false);
+
+  const handleSignIn = () => {
+    setLoading(true);
+    signIn("github", { callbackUrl: "/" });
+  };
+
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-xl">Login</CardTitle>
+          <CardDescription>
+            Login with your GitHub account to continue
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Button
+            variant="outline"
+            className="w-full flex items-center space-x-3"
+            onClick={handleSignIn}
+            disabled={loading}
+          >
+            {loading ? (
+              <span>Loading...</span>
+            ) : (
+              <>
+                <Github />
+                <span>Sign up with GitHub</span>
+              </>
+            )}
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default LoginForm;
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,54 +1,13 @@
-"use client";
+import { Metadata } from "next";
+import LoginForm from "./_component/login-form";
 
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import Github from "@/components/icons/Github";
-import { signIn } from "next-auth/react";
+export const metadata: Metadata = {
+  title: "Login",
+  description: "Login with your GitHub account to continue",
+};
 
 const LoginPage = () => {
-  const [loading, setLoading] = useState(false);
-
-  const handleSignIn = () => {
-    setLoading(true);
-    signIn("github", { callbackUrl: "/" });
-  };
-
-  return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <Card className="mx-auto max-w-sm">
-        <CardHeader>
-          <CardTitle className="text-xl">Login</CardTitle>
-          <CardDescription>
-            Login with your GitHub account to continue
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Button
-            variant="outline"
-            className="w-full flex items-center space-x-3"
-            onClick={handleSignIn}
-            disabled={loading}
-          >
-            {loading ? (
-              <span>Loading...</span>
-            ) : (
-              <>
-                <Github />
-                <span>Sign up with GitHub</span>
-              </>
-            )}
-          </Button>
-        </CardContent>
-      </Card>
-    </div>
-  );
+  return <LoginForm />;
 };
 
 export default LoginPage;

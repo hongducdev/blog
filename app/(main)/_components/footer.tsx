8c40f054ae3826f22442a7cc037ead7a937ca005
hongducdev <contact.hongduc@gmail.com>
2024-06-11 20:27:09 +0700

    refactor: Update footer component to include copyright information and DMCA protection badge
    The footer component in the app/(main)/_components/footer.tsx file has been updated to include copyright information and a DMCA protection badge. This change ensures that the footer displays the necessary legal information and provides a visual indicator of DMCA protection.

--- app/(main)/_components/footer.tsx
+++ app/(main)/_components/footer.tsx
@@ -5,30 +5,35 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="border-y">
-      <div className="max-w-7xl mx-auto flex flex-col items-center p-3 text-xs lg:text-sm">
-        <span className="flex items-center gap-2">
-          Bản quyền thuộc về ©{" "}
-          <Link
-            href="https://hongducdev.com/"
-            className="text-green-500 hover:underline"
-          >
-            hongducdev
-          </Link>
-          <Link
-            href="//www.dmca.com/Protection/Status.aspx?ID=40e26100-615b-40a0-b601-8fbf47d55f30"
-            title="DMCA.com Protection Status"
-            className="dmca-badge"
-          >
-            {" "}
-            <Image
-              src="https://images.dmca.com/Badges/dmca_protected_sml_120m.png?ID=40e26100-615b-40a0-b601-8fbf47d55f30"
-              alt="DMCA.com Protection Status"
-              width={120}
-              height={24}
-            />
-          </Link>
-        </span>
-        <div className="lg:flex items-center space-x-2 hidden">
+      <div className="max-w-7xl mx-auto flex flex-col py-12 px-3 text-xs lg:text-sm">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="">
+            <h5 className="text-xl font-semibold">Bản quyền</h5>
+            <div className="mt-5">
+              <Link
+                href="/policies"
+                className="text-base font-medium hover:underline"
+              >
+                Thông tin về bản quyền
+              </Link>
+              <Link
+                href="//www.dmca.com/Protection/Status.aspx?ID=40e26100-615b-40a0-b601-8fbf47d55f30"
+                title="DMCA.com Protection Status"
+                className="dmca-badge mt-2"
+              >
+                {" "}
+                <Image
+                  src="https://images.dmca.com/Badges/dmca_protected_sml_120m.png?ID=40e26100-615b-40a0-b601-8fbf47d55f30"
+                  alt="DMCA.com Protection Status"
+                  width={120}
+                  height={24}
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="lg:flex items-center justify-center space-x-2 hidden lg:mt-10 text-center">
           <p>Powered by</p>
           <Link
             href="https://nextjs.org/"

97312bd6f38171357bfdcf9de4960be548de1a51
hongducdev <contact.hongduc@gmail.com>
2024-06-15 07:28:01 +0700

    refactor: Update api GET and POST Tag

--- app/api/tags/route.ts
+++ app/api/tags/route.ts
@@ -1,20 +1,10 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prismadb";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/config/authOptions";
 
-// Existing GET method
 export const GET = async () => {
   try {
-    const posts = await prisma.post.findMany({
-      orderBy: {
-        createdAt: "desc",
-      },
-      where: {
-        isDeleted: false,
-      },
-    });
-    return NextResponse.json(posts);
+    const tags = await prisma.tag.findMany();
+    return NextResponse.json(tags);
   } catch (error: unknown) {
     const errorMessage =
       error instanceof Error ? error.message : "An unknown error occurred";
@@ -22,26 +12,10 @@ export const GET = async () => {
   }
 };
 
-// POST method with authentication
 export const POST = async (req: Request) => {
-  const session = await getServerSession(authOptions);
+  const { name } = await req.json();
 
-  if (!session) {
-    return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
-  }
-
-  const { thumbnail, title, slug, icon, tag, content, shortDesc } =
-    await req.json();
-
-  if (
-    !thumbnail ||
-    !title ||
-    !slug ||
-    !icon ||
-    !tag ||
-    !content ||
-    !shortDesc
-  ) {
+  if (!name) {
     return NextResponse.json(
       { error: "Missing required fields." },
       { status: 400 }
@@ -49,99 +23,13 @@ export const POST = async (req: Request) => {
   }
 
   try {
-    const post = await prisma.post.create({
+    const tag = await prisma.tag.create({
       data: {
-        thumbnail,
-        title,
-        slug,
-        icon,
-        tag,
-        content,
-        shortDesc,
+        tagName: name,
       },
     });
-    return NextResponse.json(post);
-  } catch (error: unknown) {
-    const errorMessage =
-      error instanceof Error ? error.message : "An unknown error occurred";
-    return NextResponse.json({ error: errorMessage }, { status: 500 });
-  }
-};
-
-// PUT method with authentication
-export const PUT = async (req: Request) => {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
-  }
-
-  const { id, thumbnail, title, slug, icon, tag, content, shortDesc } =
-    await req.json();
-
-  if (
-    !id ||
-    !thumbnail ||
-    !title ||
-    !slug ||
-    !icon ||
-    !tag ||
-    !content ||
-    !shortDesc
-  ) {
-    return NextResponse.json(
-      { error: "Missing required fields." },
-      { status: 400 }
-    );
-  }
-
-  try {
-    const post = await prisma.post.update({
-      where: {
-        id,
-      },
-      data: {
-        thumbnail,
-        title,
-        slug,
-        icon,
-        tag,
-        content,
-        shortDesc,
-      },
-    });
-    return NextResponse.json(post);
-  } catch (error: unknown) {
-    const errorMessage =
-      error instanceof Error ? error.message : "An unknown error occurred";
-    return NextResponse.json({ error: errorMessage }, { status: 500 });
-  }
-};
-
-// DELETE method with authentication
-export const DELETE = async (req: Request) => {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
-  }
-
-  const { id } = await req.json();
-
-  if (!id) {
-    return NextResponse.json(
-      { error: "Missing required fields." },
-      { status: 400 }
-    );
-  }
-
-  try {
-    const post = await prisma.post.update({
-      where: { id },
-      data: { isDeleted: true },
-    });
-    return NextResponse.json(post);
-  } catch (error: unknown) {
+    return NextResponse.json(tag);
+  } catch (error) {
     const errorMessage =
       error instanceof Error ? error.message : "An unknown error occurred";
     return NextResponse.json({ error: errorMessage }, { status: 500 });
